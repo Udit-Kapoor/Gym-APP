6ee359751d9ca77628353da10ab1c4a35672b739
Aman Nanda <34944818+amannanda8@users.noreply.github.com>
2021-02-04 20:05:39 +0530

    added customer homescreen

--- lib/routes.dart
+++ lib/routes.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/widgets.dart';
-
-import 'views/customer/customer_drawer/customer_profile_view.dart';
+import 'package:gym_app/views/views.dart';
 
 class Routes {
   Map<String, Widget Function(BuildContext)> _routes = {
-    '/': (c) => CustomerProfileView(),
+    //'/': (c) => CustomerProfileView(),
+    '/': (c) => Home(),
   };
 
   Map<String, Widget Function(BuildContext)> get routes => _routes;
---
+++ lib/views/home/my_attendance.dart
@@ -0,0 +1,108 @@
+import 'package:flutter/material.dart';
+
+class MyAttendance extends StatelessWidget {
+  const MyAttendance({
+    Key key,
+    @required this.height,
+  }) : super(key: key);
+
+  final double height;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: height * 0.4,
+      child: Column(
+        mainAxisSize: MainAxisSize.max,
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          Text(
+            'My Attendance',
+            style: Theme.of(context)
+                .textTheme
+                .headline5
+                .copyWith(fontWeight: FontWeight.bold, color: Colors.black),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              GestureDetector(
+                child: Icon(
+                  Icons.arrow_back_ios,
+                  size: 20.0,
+                ),
+                onTap: () {},
+              ),
+              Text('October'),
+              GestureDetector(
+                child: RotatedBox(
+                  quarterTurns: 2,
+                  child: Icon(
+                    Icons.arrow_back_ios,
+                    size: 20.0,
+                  ),
+                ),
+                onTap: () {},
+              ),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              Flexible(
+                child: Container(
+                  height: height * 0.3,
+                  child: ListView.builder(
+                    itemCount: 30,
+                    itemBuilder: (BuildContext context, int index) {
+                      return ListTile(
+                        leading: Text(
+                          'Day\n$index',
+                          textAlign: TextAlign.center,
+                        ),
+                        trailing: index % 2 == 0
+                            ? Icon(Icons.check_circle_outline,
+                                color: Colors.green)
+                            : Icon(Icons.cancel_outlined, color: Colors.red),
+                      );
+                    },
+                  ),
+                ),
+              ),
+              SizedBox(
+                width: 40,
+              ),
+              Flexible(
+                fit: FlexFit.tight,
+                child: Container(
+                  height: height * 0.3,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Icon(
+                        Icons.donut_large,
+                        size: 40.0,
+                      ),
+                      Text(
+                        'Total\nAttendance',
+                        textAlign: TextAlign.center,
+                      ),
+                      Icon(
+                        Icons.donut_large,
+                        size: 40.0,
+                      ),
+                      Text(
+                        'Attendance\nPerc.',
+                        textAlign: TextAlign.center,
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
