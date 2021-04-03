db782c28cf0777c5302462074ae2adf6de901ee9
Aman Nanda <34944818+amannanda8@users.noreply.github.com>
2021-04-03 08:54:05 +0530

    integrated customer drawer

--- lib/views/customer/drawer/customer_drawer.dart
+++ lib/views/customer/drawer/customer_drawer.dart
@@ -41,7 +41,26 @@ class _CustomerDrawerState extends State<CustomerDrawer> {
             height: widget.height * 0.03,
           ),
         
-         
+          FutureBuilder(
+              future: customerPro(),
+              builder: (c, s) {
+                if (s.connectionState == ConnectionState.waiting)
+                  return CircularProgressIndicator();
+                else if(s.connectionState == ConnectionState.done){
+
+                  var cp = customerProfileModelFromJson(s.data.data);
+                  return ListTile(
+                    contentPadding: EdgeInsets.all(0),
+                    leading: Image.network(
+                      baseURL + cp.photo??"/media/customer/photo/badge.png",
+                      alignment: Alignment.topLeft,
+                    ),
+                    title: Text('Hi! ${cp.firstName}'),
+                    subtitle: Text('#'+ cp.cid),
+                  );
+                }
+                return Center(child: Text('Oops no data found'));
+              }),
           DrawerTextList(
               label: 'Profile',
               onTap: () {
